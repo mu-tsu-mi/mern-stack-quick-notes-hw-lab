3d5905bda97e7093af03dd54ad9eea45ec662c5e
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-07-26 18:42:08 +1000

    fix prop for log in, add log-in and sign-up flip

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,28 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from '../../components/LoginForm/LoginForm';
+import { useState } from 'react';
 
 export default function AuthPage({setUser}) {
-    return (
-      <main>
-        <h1>AuthPage</h1>
-        <SignUpForm setUser={setUser} />
-        <LoginForm />
-      </main>
-    );
+  const [flip, setFlip] = useState(true)
+  
+  function handleFlipping() {
+    setFlip(!flip)
   }
+
+  return (
+    <main>
+      <h1>Welcome</h1>
+      { flip ? 
+        <div>
+          <SignUpForm setUser={setUser} />
+          <button onClick={handleFlipping}>Show Log-in</button>
+        </div>
+        :
+        <div>
+          <LoginForm setUser={setUser} />
+          <button onClick={handleFlipping}>Show Sign-up</button>
+        </div>
+      }
+    </main>
+  );
+}
